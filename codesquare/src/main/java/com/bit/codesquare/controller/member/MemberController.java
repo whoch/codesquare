9bed38ada1efba0f61af2d85a8e34640faeaf3dc
jiyeon <jiyeonk1227@gmail.com>
2019-02-08 17:08:26 +0900

    12

--- codesquare/src/main/java/com/bit/codesquare/controller/member/MemberController.java
+++ codesquare/src/main/java/com/bit/codesquare/controller/member/MemberController.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
