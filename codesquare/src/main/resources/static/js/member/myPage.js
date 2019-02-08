2b596236a4828d2aad7ea9c4ba3b6d21325b9754
jiyeon <jiyeonk1227@gmail.com>
2019-02-08 14:50:50 +0900

    12

--- codesquare/src/main/resources/static/js/member/myPage.js
+++ codesquare/src/main/resources/static/js/member/myPage.js
@@ -281,36 +281,5 @@ $(function(){
 
 
 }); // end function
-// 비밀번호 찾기 버튼 클릭시
-// $("#findPwBar").click(function(){
-// $.ajax({ㄴ
-// type: 'post',
-// url : 'findPw',
-// dataType : 'html',
-// success : function(data) {
-// //alert("pw");
-// $("#content").html(data);
-// }
-// });
-// })
-//
-//	
-// function emailValidate() {
-//
-// var email = $("#email").val();
-// var regExp = /[a-zA-Z0-9]{2,}@[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,}/i;
-//
-// if (regExp.test(email)) {
-// findIdDone();
-// } else {
-// $("#emailCheckMsg").css("color", "red");
-// $("#emailCheckMsg").text("올바르지 않는 이메일 형식입니다.");
-// $("#email").focus();
-// }
-//
-// };
-// function findIdDone() {
-//	
-// }
-//
+
 
