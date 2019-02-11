c48bc08858e98bcd68cfc1f3e65460dba50467e1
ckdrha <life5248@naver.com>
2019-02-12 00:38:35 +0900

    룸예약 커밋입니다 -창형-

--- codesquare/src/main/java/com/bit/codesquare/controller/room/ReservationController.java
+++ codesquare/src/main/java/com/bit/codesquare/controller/room/ReservationController.java
@@ -3,27 +3,38 @@ package com.bit.codesquare.controller.room;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bit.codesquare.dto.room.Reservation;
 import com.bit.codesquare.dto.room.Room;
+import com.bit.codesquare.mapper.room.ReservationMapper;
 import com.bit.codesquare.mapper.room.RoomMapper;
+
 
 @Controller
 public class ReservationController {
-
+	
+	private static final Logger Logger = LoggerFactory.getLogger(ReservationController.class);
 	@Autowired
 	RoomMapper roomMapper;
-	
+	@Autowired
+	ReservationMapper reservationMapper;
 	
 	@Value("${roomIntro.savepath.directory}")
 	String path;
 	
 	List<Room> licList = new ArrayList<Room>();
+	List<Room> roomview = new ArrayList<Room>();
 	
 	@RequestMapping("/getid")
 	public ModelAndView getid(Model model, String companyid) throws Exception {
@@ -40,6 +51,46 @@ public class ReservationController {
 		return new ModelAndView("room/roomList", "list", licList);
 	}
 	
+	
+	@RequestMapping("/roomView")
+	public ModelAndView roomView(Model model, String id) throws Exception {
+		try {
+			roomview=roomMapper.getroom(id);
+			for(Room l:roomview) {
+				String thumbPath=path;
+				thumbPath+=l.getId()+"/Thumbnail."+l.getExtension();
+				l.setThumbnailPath(thumbPath);
+			}
+		}catch (Exception e) {
+			
+		}
+		
+//		int[] test = new int[] {8,9,11,15,16};
+		String[] test = new String[] {"03:00:00","08:00:00"};
+		model.addAttribute("test", test);
+		
+		System.out.println(roomview.toString());
+		return new ModelAndView("room/roomView", "list", roomview);
+	}
+	
+	@RequestMapping("/reserve")
+	public String reserve(HttpServletRequest request, Model model, @ModelAttribute Reservation reservation) throws Exception {
+//		int no = Integer.parseInt(request.getParameter("no"));
+		Logger.info("하기전:"+reservation.getNo());
+
+		reservationMapper.insert(reservation);
+		reservationMapper.enddate(reservation.getNo());
+
+		Logger.info("와야됨:"+reservation.getNo());
+		return "redirect:companyList";
+	}
+}
+//	@RequestMapping("/roomView")
+//	public String roomView(Model model, String id) throws Exception {
+//		model.addAttribute("list", roomMapper.getroom(id));
+//		return "room/roomView";
+//	}
+
 //	@RequestMapping("/getid")
 //	public String getid(Model model, String companyid) throws Exception {
 //		model.addAttribute("list", roomMapper.getid(companyid));
@@ -52,7 +103,7 @@ public class ReservationController {
 //		}
 //		return "roomList";
 //	}
-}
+
 //	@RequestMapping("/roomList")
 //	public String getAllRoom(Model model) throws Exception {
 //		
---
+++ codesquare/src/main/java/com/bit/codesquare/dto/room/Reservation.java
@@ -0,0 +1,24 @@
+package com.bit.codesquare.dto.room;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class Reservation {
+
+	int no;
+	String roomid;
+	int boardid;
+	String userid;
+	Date reservationdate;
+	Timestamp paymentdate;
+	int payamount;
+	Time reservationtime;
+	Time usetime;
+	int person;
+	Date enddate;
+	
+}
