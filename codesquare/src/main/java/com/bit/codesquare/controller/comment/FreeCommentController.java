7b47db8901095bb1a61d4deb9ed401afc726c70c
ckdrha <life5248@naver.com>
2019-03-04 12:10:38 +0900

    알림 css 수정 / 댓글 닉네임 추가

--- codesquare/src/main/java/com/bit/codesquare/controller/comment/FreeCommentController.java
+++ codesquare/src/main/java/com/bit/codesquare/controller/comment/FreeCommentController.java
@@ -1,6 +1,8 @@
 package com.bit.codesquare.controller.comment;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,12 +36,13 @@ public class FreeCommentController {
     
     @RequestMapping("/insert") //댓글 작성 
     @ResponseBody
-    private int mCommentServiceInsert(@RequestParam int bno, @RequestParam String content) throws Exception{
+    private int mCommentServiceInsert(@RequestParam int bno, @RequestParam String content, HttpSession session) throws Exception{
+    	String nickName = (String) session.getAttribute("nickName");
         ReplyDto comment = new ReplyDto();
         comment.setBno(bno);
         comment.setContent(content);
         //로그인 기능을 구현했거나 따로 댓글 작성자를 입력받는 폼이 있다면 입력 받아온 값으로 사용하면 됩니다. 저는 따로 폼을 구현하지 않았기때문에 임시로 "test"라는 값을 입력해놨습니다.
-        comment.setWriter("test");  
+        comment.setWriter(nickName);  
         return replyMapper.commentInsert(comment);
     }
     
